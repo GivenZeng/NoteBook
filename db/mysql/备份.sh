a1f6a03020fc88d785d4de72353f91424da7e894
曾纪文 <zengjiwen@bytedance.com>
2020-12-31 20:36:26 +0800

    rv

--- db/mysql/备份.sh
+++ db/mysql/备份.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/mysqldump -u root -pbytedance --all-databases > `date +'%Y-%m-%d-%H'`.sql
+/usr/bin/mysqldump -u root -pyourpasspord --all-databases > `date +'%Y-%m-%d-%H'`.sql
